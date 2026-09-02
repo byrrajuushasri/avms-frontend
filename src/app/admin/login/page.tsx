a5a14c576fd1878b893b1467327c65c582f4f7cb
ramanaraju2025 <ramanaraju2025@gmail.com>
2026-09-02 19:06:56 +0530

    update

--- src/app/admin/login/page.tsx
+++ src/app/admin/login/page.tsx
@@ -30,9 +30,8 @@ export default function LoginPage() {
   ========================================================= */
 
   const BACKEND_URL =
-    process.env.NEXT_PUBLIC_API_URL ||
-    "http://localhost:5000";
-
+  process.env.NEXT_PUBLIC_BACKEND_URL ||
+  "http://localhost:5000";
   /* =========================================================
      LOGIN
   ========================================================= */
